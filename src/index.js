9837a8bf120dccceca6658b6536c39e357eb684f
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-11-06 19:37:46 -0300

    get /tweets

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {json} from 'express';
 import cors from 'cors';
 
 const app = express();
@@ -11,41 +11,99 @@ const tweets = [
         username: "bobesponja",
         avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
         tweet: "eu amo o hub"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
+    },
+    {
+        username: "bobesponja",
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+        tweet: "eu amo o hub"
     }
 ]
 
 app.post('/sign-up', (req, res) => {
     const {username, avatar} = req.body;
+    
     if(!username || !avatar){
         res.status(420).send("Please, fill all fields");
         return;
-    }
-    const signedUser = users.find((usr)=> usr.username === username);
-    if(signedUser){
+    }    
+ /*    if(signedUser){
         res.status(409).send("User already exists");
         return;
-    }
-        
+    } */
+    const user = {username, avatar};
 
-    users.push(req.body);
+    users.push(user);
     console.log(users);
     res.status(201).send("OK!");
 })
 
 app.post('/tweets', (req, res)=>{
     const {username, tweet} = req.body;
-    
+
+    const usr = users[0];
+
+    const obj = {
+        username, 
+        tweet,
+        avatar: usr.avatar
+    }
+
     if(!username || !tweet){
         res.status(420).send("Please, fill all fields");
         return;
     }
+    
 
-    tweets.push(req.body);
-    console.log(tweets);
+    //tweets.push(req.body);
+    tweets.unshift(req.body);
     res.status(201).send("OK!");
 
 })
 
-console.log(users)
+app.get('/tweets', (req,res)=>{
+    let tweetList = [];
+
+	for(let i = 0; i < 10; i++){
+		tweetList.push(tweets[i]);
+	}
+
+	res.send(tweetList);
+    
+})
 
 app.listen(5000);
