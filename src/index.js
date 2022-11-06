24f5a04b9f34e9b521765e04d21840b1230dc3d6
Gustavo de Assis <gustavo.siqueira.421@gmail.com>
2022-11-06 11:29:02 -0300

    post /tweets

--- src/index.js
+++ src/index.js
@@ -11,36 +11,6 @@ const tweets = [
         username: "bobesponja",
         avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
         tweet: "eu amo o hub"
-    },
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
-    },
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
-    },
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
-    },
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
-    },
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
-    },
-    {
-        username: "bobesponja",
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-        tweet: "eu amo o hub"
     }
 ]
 
@@ -59,7 +29,21 @@ app.post('/sign-up', (req, res) => {
 
     users.push(req.body);
     console.log(users);
-    res.status(201).send("Created user with success!");
+    res.status(201).send("OK!");
+})
+
+app.post('/tweets', (req, res)=>{
+    const {username, tweet} = req.body;
+    
+    if(!username || !tweet){
+        res.status(420).send("Please, fill all fields");
+        return;
+    }
+
+    tweets.push(req.body);
+    console.log(tweets);
+    res.status(201).send("OK!");
+
 })
 
 console.log(users)
